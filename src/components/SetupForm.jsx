bc34ba0515e039558d6743b972b1c566f9049a34
Kamchybekk <manasbekoovich@gmail.com>
2024-01-25 18:12:37 +0600

    I added the initial options and also added a radio button.

--- src/components/SetupForm.jsx
+++ src/components/SetupForm.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from './Modal';
-import { Button, styled, FormControl, TextField } from '@mui/material';
+import {
+  Button,
+  styled,
+  FormControl,
+  TextField,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from '@mui/material';
 import CreatableSelect from 'react-select/creatable';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -32,6 +41,7 @@ const getTweetsFromHistory = async (username) => {
 };
 
 function SetupForm({ openModal, setOpenModal, username, resetUsername }) {
+  const [broadcastType, setBroadcastType] = useState('public');
   const formik = useFormik({
     initialValues: { selectedRelays: [] },
     validationSchema: validationSchemaForRelays,
@@ -51,14 +61,54 @@ function SetupForm({ openModal, setOpenModal, username, resetUsername }) {
     },
   });
 
+  const handleBroadcastTypeChange = (event) => {
+    setBroadcastType(event.target.value);
+    if (event.target.value === 'public') {
+      formik.setFieldValue('selectedRelays', []);
+    }
+  };
+
   return (
     <div>
       <ToastContainer />
       <Modal open={openModal} onClose={() => setOpenModal(false)}>
         <FormStyled onSubmit={formik.handleSubmit}>
+          <label>Broadcast Type</label>
+          <RadioGroup
+            row
+            className="radio-group-block"
+            value={broadcastType}
+            onChange={handleBroadcastTypeChange}
+            name="broadcastType"
+          >
+            <FormControlLabel
+              value="public"
+              control={<Radio />}
+              label="Broadcast to public relays"
+            />
+            <FormControlLabel
+              value="specific"
+              control={<Radio />}
+              label="Broadcast to specific relays"
+            />
+          </RadioGroup>
+
+          <label>Bunker url</label>
+          <TextField
+            fullWidth
+            size="small"
+            variant="outlined"
+            value={formik.values.bunkerUrl}
+            {...formik.getFieldProps('username')}
+            placeholder="Bunker url"
+          />
+          <span className="description-bunkerUrl">
+            Paste URL from your nsecbunker
+          </span>
           <label>Relays</label>
           <CreatableSelect
             isMulti
+            isDisabled={broadcastType === 'public'}
             name="selectedRelays"
             classNamePrefix="select"
             placeholder="Relays"
@@ -67,16 +117,25 @@ function SetupForm({ openModal, setOpenModal, username, resetUsername }) {
             styles={customStyles(
               formik.errors.selectedRelays && formik.touched.selectedRelays
             )}
+            options={[
+              { value: 'wss://nos.lol', label: 'wss://nos.lol' },
+              { value: 'wss://relay.exit.pub', label: 'wss://relay.exit.pub' },
+              {
+                value: 'wss://nostr.mutinywallet.com',
+                label: 'wss://nostr.mutinywallet.com',
+              },
+              { value: 'wss://relay.damus.io', label: 'wss://relay.damus.io' },
+              {
+                value: 'wss://relay.nostr.band',
+                label: 'wss://relay.nostr.band',
+              },
+              {
+                value: 'wss://nostr.mom',
+                label: 'wss://nostr.mom',
+              },
+            ]}
           />
 
-          <TextField
-            fullWidth
-            size="small"
-            variant="outlined"
-            value={formik.values.bunkerUrl}
-            onChange={(value) => formik.setFieldValue('bunkerUrl', value)}
-            placeholder="Bunker url"
-          />
           <StyledButton size="small" type="submit" variant="contained">
             OK
           </StyledButton>
@@ -93,6 +152,7 @@ const FormStyled = styled('form')`
   flex-direction: column;
   gap: 10px;
   font-family: 'Raleway', sans-serif;
+  padding: 15px 5px 15px 5px;
   .MuiInputBase-input {
     font-family: 'Raleway', sans-serif;
     padding-left: 10px;
@@ -100,6 +160,16 @@ const FormStyled = styled('form')`
       color: #808080;
       opacity: 1;
     }
+  }
+  .description-bunkerUrl {
+    font-size: 0.9rem;
+    color: #616060;
+  }
+  .MuiFormControlLabel-root {
+    height: 30px;
+  }
+  .MuiTypography-root {
+    font-family: 'Raleway', sans-serif;
   }
 `;
 
