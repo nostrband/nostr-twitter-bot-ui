6b3e0a055dadbcd054f09ecabbcf1d971b9eb2bd
artur <brugeman.artur@gmail.com>
2024-01-26 11:04:29 +0300

    Fix bunkerUrl, add error toast, add submitting info, add username hidden input,

--- src/components/SetupForm.jsx
+++ src/components/SetupForm.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Modal from './Modal';
+import React, { useState } from "react";
+import Modal from "./Modal";
 import {
   Button,
   styled,
@@ -9,22 +9,22 @@ import {
   RadioGroup,
   FormControlLabel,
   Radio,
-} from '@mui/material';
-import CreatableSelect from 'react-select/creatable';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
-import { useFormik } from 'formik';
-import { validationSchemaForRelays } from '../helpers/validations';
-import { API_ENDPOINT } from '../helpers/constants';
+} from "@mui/material";
+import CreatableSelect from "react-select/creatable";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { useFormik } from "formik";
+import { validationSchemaForRelays } from "../helpers/validations";
+import { API_ENDPOINT } from "../helpers/constants";
 
 const customStyles = (error) => ({
   control: (provided) => ({
     ...provided,
-    borderColor: error ? 'red' : provided.borderColor,
-    boxShadow: error ? '0 0 0 1px red' : provided.boxShadow,
-    '&:hover': {
-      borderColor: error ? 'red' : provided.borderColor,
+    borderColor: error ? "red" : provided.borderColor,
+    boxShadow: error ? "0 0 0 1px red" : provided.boxShadow,
+    "&:hover": {
+      borderColor: error ? "red" : provided.borderColor,
     },
   }),
 });
@@ -34,38 +34,50 @@ const getTweetsFromHistory = async (username) => {
     const response = await axios.get(
       `${API_ENDPOINT}/history?username=${username}`
     );
-    return console.log(response, 'RESPONSE');
+    return console.log(response, "RESPONSE");
   } catch (error) {
-    return console.log(error, 'ERROR');
+    return console.log(error, "ERROR");
   }
 };
 
 function SetupForm({ openModal, setOpenModal, username, resetUsername }) {
-  const [broadcastType, setBroadcastType] = useState('public');
+  const [broadcastType, setBroadcastType] = useState("public");
   const formik = useFormik({
-    initialValues: { selectedRelays: [] },
+    initialValues: { selectedRelays: [], bunkerUrl: "" },
     validationSchema:
-      broadcastType !== 'public' ? validationSchemaForRelays : null,
+      broadcastType !== "public" ? validationSchemaForRelays : null,
     onSubmit: async (values) => {
       try {
+        console.log({ values });
         const response = await axios.post(`${API_ENDPOINT}/add`, {
           username,
           relays: values.selectedRelays.map((item) => item.value),
+          bunkerUrl: values.bunkerUrl,
         });
-        toast.success('Success!');
+        toast.success("Success!");
         resetUsername();
         getTweetsFromHistory(username);
         setOpenModal(false);
       } catch (error) {
-        toast.error('Error: ' + error.message);
+        switch (error.response?.status) {
+          case 400:
+          case 403:
+          case 405:
+          case 500:
+            toast.error("Error: " + error.response.data);
+            break;
+          default:
+            toast.error("Error: " + error.message);
+        }
+        console.log({ error });
       }
     },
   });
 
   const handleBroadcastTypeChange = (event) => {
     setBroadcastType(event.target.value);
-    if (event.target.value === 'public') {
-      formik.setFieldValue('selectedRelays', []);
+    if (event.target.value === "public") {
+      formik.setFieldValue("selectedRelays", []);
     }
   };
 
@@ -74,7 +86,9 @@ function SetupForm({ openModal, setOpenModal, username, resetUsername }) {
       <ToastContainer />
       <Modal open={openModal} onClose={() => setOpenModal(false)}>
         <FormStyled onSubmit={formik.handleSubmit}>
-          <label>Broadcast Type</label>
+          <input type="hidden" {...formik.getFieldProps("username")} />
+
+          <label>Publishing:</label>
           <RadioGroup
             row
             className="radio-group-block"
@@ -85,63 +99,74 @@ function SetupForm({ openModal, setOpenModal, username, resetUsername }) {
             <FormControlLabel
               value="public"
               control={<Radio />}
-              label="Broadcast to public relays"
+              label="To default public relays"
             />
             <FormControlLabel
               value="specific"
               control={<Radio />}
-              label="Broadcast to specific relays"
+              label="To specific relays"
             />
           </RadioGroup>
 
-          <label>Bunker url</label>
-          <TextField
-            fullWidth
-            size="small"
-            variant="outlined"
-            value={formik.values.bunkerUrl}
-            {...formik.getFieldProps('username')}
-            placeholder="Bunker url"
-          />
-          <span className="description-bunkerUrl">
-            Paste URL from your nsecbunker
-          </span>
-          <label>Relays</label>
+          <label>Relays:</label>
           <CreatableSelect
             isMulti
-            isDisabled={broadcastType === 'public'}
+            isDisabled={broadcastType === "public"}
             name="selectedRelays"
             classNamePrefix="select"
-            placeholder="Relays"
-            onChange={(value) => formik.setFieldValue('selectedRelays', value)}
+            placeholder="Enter relay urls"
+            onChange={(value) => formik.setFieldValue("selectedRelays", value)}
             value={formik.values.selectedRelays}
             styles={customStyles(
               formik.errors.selectedRelays &&
                 formik.touched.selectedRelays &&
-                broadcastType !== 'public'
+                broadcastType !== "public"
             )}
             options={[
-              { value: 'wss://nos.lol', label: 'wss://nos.lol' },
-              { value: 'wss://relay.exit.pub', label: 'wss://relay.exit.pub' },
+              { value: "wss://nos.lol", label: "wss://nos.lol" },
+              { value: "wss://relay.exit.pub", label: "wss://relay.exit.pub" },
               {
-                value: 'wss://nostr.mutinywallet.com',
-                label: 'wss://nostr.mutinywallet.com',
+                value: "wss://nostr.mutinywallet.com",
+                label: "wss://nostr.mutinywallet.com",
               },
-              { value: 'wss://relay.damus.io', label: 'wss://relay.damus.io' },
+              { value: "wss://relay.damus.io", label: "wss://relay.damus.io" },
               {
-                value: 'wss://relay.nostr.band',
-                label: 'wss://relay.nostr.band',
+                value: "wss://relay.nostr.band",
+                label: "wss://relay.nostr.band",
               },
               {
-                value: 'wss://nostr.mom',
-                label: 'wss://nostr.mom',
+                value: "wss://nostr.mom",
+                label: "wss://nostr.mom",
               },
             ]}
           />
 
-          <StyledButton size="small" type="submit" variant="contained">
-            OK
+          <label>Connect to your keys:</label>
+          <TextField
+            fullWidth
+            size="small"
+            variant="outlined"
+            name="bunkerUrl"
+            value={formik.values.bunkerUrl.value}
+            onChange={(value) =>
+              formik.setFieldValue("bunkerUrl", value.target.value)
+            }
+            placeholder="Bunker url"
+          />
+          <span className="description-bunkerUrl">
+            Leave blank to keep the existing connection.
+          </span>
+
+          <StyledButton disabled={formik.isSubmitting} size="small" type="submit" variant="contained">
+            Setup
           </StyledButton>
+
+          {formik.isSubmitting && (
+            <span className="description-bunkerUrl">
+              Connecting to your keys...<br/>
+              Please confirm key access in your key storage app!
+            </span>
+          )}
         </FormStyled>
       </Modal>
     </div>
@@ -150,14 +175,14 @@ function SetupForm({ openModal, setOpenModal, username, resetUsername }) {
 
 export default SetupForm;
 
-const FormStyled = styled('form')`
+const FormStyled = styled("form")`
   display: flex;
   flex-direction: column;
   gap: 10px;
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   padding: 15px 5px 15px 5px;
   .MuiInputBase-input {
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
     padding-left: 10px;
     &::placeholder {
       color: #808080;
@@ -172,7 +197,7 @@ const FormStyled = styled('form')`
     height: 30px;
   }
   .MuiTypography-root {
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
   }
 `;
 
